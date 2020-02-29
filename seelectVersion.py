6d82ce497294f9db6f3cc52716bf83f77a4fabbf
liupeng <liupeng.0@outlook.com>
2020-02-29 23:14:07 +0800

    create empty V 1.0.2

--- seelectVersion.py
+++ seelectVersion.py
@@ -1,3 +1,29 @@
 import torch
 
 print(torch.__version__)
+
+x = torch.empty(5, 3)
+print(x)
+
+x = torch.zeros(5, 3, dtype=torch.long)
+print(x)
+
+x = torch.tensor([5.5, 3, 4, 5])
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
